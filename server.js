14613a79cbeacd99e9e08edd05ca7e5d93b002c4
deijahmonay <deijah@shack15.com>
2024-10-02 21:34:55 -0700

    Initialize express with mongodb, middleware and dynamc port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// IMPORTS
 require('dotenv').config();
 const express = require("express");
 const app = express();
@@ -6,6 +7,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+// PORT
 const port = process.env.PORT || "4000";
 
 moongoose.connect(process.env.MONGODB_URI);
@@ -13,3 +15,14 @@ moongoose.connect(process.env.MONGODB_URI);
 mongoose.connect.om("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
+
+// MIDDLEWARE
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use(method("-method"));
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}.`)
+})
