73668de9bf8fb406107f350f7104ec98532a8301
deijahmonay <deijah@shack15.com>
2024-10-02 21:28:13 -0700

    Set up MongoDB connection and dynamic port config

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+
+const port = process.env.PORT || "4000";
+
+moongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connect.om("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
