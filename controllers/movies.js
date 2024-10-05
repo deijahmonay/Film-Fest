a84f83319640f939dbe97a658c6470a6825e84fe
deijahmonay <deijah@shack15.com>
2024-10-05 02:45:26 -0700

    debug views movie.ejs navbar link, views movies button(form) edit.ejs id placement

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,90 +1,79 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
 
-
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     res.render('movies/index.ejs', {
       movies: currentUser.movies,
     });
-  }catch(err){
+  } catch (err) {
     res.redirect('/');
   }
 });
 
-router.get('/new', async(req, res) => {
+router.get('/new', async (req, res) => {
   res.render('movies/new.ejs');
 });
 
-router.get('/:movieId', async(req, res) => {
+router.get('/:movieId', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     const movie = currentUser.movies.id(req.params.movieId);
-    res.render('movies/show.ejs', {
-      movie 
-    })
+    res.render('movies/show.ejs', { movie });
   } catch (err) {
     console.log(err);
     res.redirect('/');
   }
-  });
-
-  router.get('/:movieId/edit', async(req, res) => {
-    try{
-      const currentUser = await User.findById(req.session.user._id)
-      const movie = currentUser.movies.id(req.params.movieId)
-      res.render('movies/edit.ejs', {
-        movie
-      })
-    }catch(err){
-      console.log(err)
-      res.redirect('/')
-    }
-  });
-
-router.post('/', async(req,res) =>{
-  try{
-  const currentUser = await User.findById(req.session.user._id);
-  currentUser.movies.push(req.body)
-  await currentUser.save()
-  res.redirect(`/users/${currentUser._id}/movies`);
-} catch (err) {
-  console.log(error)
-  res.redirect('/')
-}
 });
 
-router.put('/:movieId', async(req,res) => {
-  try{
+router.get('/:movieId/edit', async (req, res) => {
+  try {
     const currentUser = await User.findById(req.session.user._id);
-
     const movie = currentUser.movies.id(req.params.movieId);
+    res.render('movies/edit.ejs', { movie });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+});
 
-    movie.set(req.body)
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.movies.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/movies`);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+});
 
+router.put('/:movieId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const movie = currentUser.movies.id(req.params.movieId);
+    movie.set(req.body);
     await currentUser.save();
     res.redirect(`/users/${currentUser._id}/movies/${req.params.movieId}`);
-  
-  }catch(err){
-    console.log(err)
-    res.redirect('/')
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
   }
-})
+});
 
-router.delete('/movieId', async(req,res) => {
-  try{
-    const currentUser = await User.findById(req.session.user._id)
-
-    await currentUser.save()
-
-    res.redirect(`/users/${currentUser._id}/movies`)
-  }catch(err){
-    console.log(err)
-    res.redirect('/')
+router.delete('/:movieId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.movies.id(req.params.movieId).remove();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/movies`);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
   }
-})
+});
 
 module.exports = router;
